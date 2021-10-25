0fa4304d169dc52f133dbea16692bb1cc499e7b0
NagaVenkataSaiM <2000030558@kluniversity.in>
2021-10-25 20:39:43 +0530

    added signup and login components

---
+++ hws322/src/components/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+class Login extends Component {
+  render() {
+    return (
+      <h>Login</h>
+    );
+  }
+}
+
+export default Login;
